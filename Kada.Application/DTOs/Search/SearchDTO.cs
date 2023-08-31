2d0f1576379bf4c7e0f35cfa2737ba047b24a97e
Yattux <fabid.net@gmail.com>
2023-08-31 15:23:14 +0200

    filter

---
+++ Kada.Application/DTOs/Search/SearchDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kada.Application.DTOs.Search
+{
+    public class SearchDTO
+    {
+        public int PageIndex { get; set; }
+
+        public int PageSize { get; set; }
+
+        public Dictionary<string, string> Filters { get; set; }
+    }
+}
--- Kada.Application/Feature/Fournisseur/Query/GetFournisseur/GetFournisseurQueryHandler.cs
+++ Kada.Application/Feature/Fournisseur/Query/GetFournisseur/GetFournisseurQueryHandler.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using Kada.Application.Contracts.Pesistence;
 using Kada.Application.DTOs;
+using Kada.Application.DTOs.Search;
+using Kada.Domain;
 using MediatR;
 using System;
 using System.Collections.Generic;
@@ -10,7 +12,7 @@ using System.Threading.Tasks;
 
 namespace Kada.Application.Feature.Fournisseur.Query.GetFournisseur
 {
-    public class GetFournisseurQueryHandler : IRequestHandler<GetFournisseurQuery, IReadOnlyList<FournisseurDto>>
+    public class GetFournisseurQueryHandler : IRequestHandler<GetFournisseurQuery, SearchResult<FournisseurDto>>
     {
         private IFournisseurRepository _fournisseurRepository;
         private IMapper _map;
@@ -21,10 +23,66 @@ namespace Kada.Application.Feature.Fournisseur.Query.GetFournisseur
             _map = map;
         }
 
-        public async Task<IReadOnlyList<FournisseurDto>> Handle(GetFournisseurQuery request, CancellationToken cancellationToken)
+        public async Task<SearchResult<FournisseurDto>> Handle(GetFournisseurQuery request, CancellationToken cancellationToken)
         {
-            var fournisseurList = await _fournisseurRepository.GetAllAsync();
-            return _map.Map<IReadOnlyList<FournisseurDto>>(fournisseurList);
+            return await GetFournisseurListPageAsync(request.Search.PageIndex, request.Search.PageSize, request.Search.Filters);
+        }
+
+        public async Task<SearchResult<FournisseurDto>> GetFournisseurListPageAsync(int pageIndex, int pageSize, Dictionary<string, string> filters)
+        {
+            var filteredRequest = GetFilteredQuery(filters);
+            var filteredFournisseur = (pageIndex == -1) ? filteredRequest.ToList() : filteredRequest.Skip(pageIndex * pageSize).Take(pageSize).ToList();
+            var rows = new List<FournisseurDto>();
+
+            foreach (Domain.Fournisseur fournisseur in filteredFournisseur)
+            {
+                rows.Add(new FournisseurDto
+                {
+                    Name = fournisseur.Name,
+                    LastName = fournisseur.LastName,
+                    Email = fournisseur.Email,
+                    WhatsappNumber = fournisseur.WhatsappNumber,
+                    Identifiant = fournisseur.Identifiant
+                });
+            }
+
+            return new SearchResult<FournisseurDto>
+            {
+                Page = pageIndex,
+                CountPerPage = pageSize,
+                TotalCount = filteredRequest.Count(),
+                Results = rows
+            };
+        }
+
+        public IQueryable<Domain.Fournisseur> GetFilteredQuery(Dictionary<string, string> filter)
+        {
+            IQueryable<Domain.Fournisseur> fournisseurQuery = _fournisseurRepository.GetQuery();
+
+            foreach (var key in filter.Keys)
+            {
+                if (string.IsNullOrEmpty(filter[key]))
+                {
+                    continue;
+                }
+
+                switch (key)
+                {
+                    case "name":
+                        fournisseurQuery = _fournisseurRepository.FilterQuery(fournisseurQuery, x => x.Name.StartsWith(filter[key]));
+                        break;
+                    case "lastName":
+                        fournisseurQuery = _fournisseurRepository.FilterQuery(fournisseurQuery, x => x.LastName.StartsWith(filter[key]));
+                        break;
+                    case "whatsappNumber":
+                        fournisseurQuery = _fournisseurRepository.FilterQuery(fournisseurQuery, x => x.WhatsappNumber.StartsWith(filter[key]));
+                        break;
+                    case "identifiant":
+                        fournisseurQuery = _fournisseurRepository.FilterQuery(fournisseurQuery, x => x.Identifiant.StartsWith(filter[key]));
+                        break;
+                }
+            }
+            return fournisseurQuery;
         }
     }
 }
