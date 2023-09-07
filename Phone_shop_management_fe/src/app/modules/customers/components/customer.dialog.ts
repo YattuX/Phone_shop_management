64b86310f8003180eea06c3ce68d9358e2c1aeb5
Affana Barry <affana@netsengroup.com>
2023-09-07 18:00:10 +0000

    crud client

--- Phone_shop_management_fe/src/app/modules/customers/components/customer.dialog.ts
+++ Phone_shop_management_fe/src/app/modules/customers/components/customer.dialog.ts
@@ -8,46 +8,60 @@ import { KadaService } from 'src/app/shared/services/kada.service';
   selector: 'app-add-customer',
   templateUrl: './customer.dialog.html',
 })
-export class CustomerDialog{
-  form : FormGroup;
-  showSpinner : boolean = false;
+export class CustomerDialog {
+  form: FormGroup;
+  showSpinner: boolean = false;
   constructor(
     public dialogRef: MatDialogRef<CustomerDialog>,
     @Optional() @Inject(MAT_DIALOG_DATA) public data: any,
     private _formBuilder: FormBuilder,
     private _toastr: ToastrService,
     private _kadaService: KadaService,
-  ){
+  ) {
     this.form = this._formBuilder.group({
-      name:[null, [Validators.required, Validators.maxLength(100), Validators.minLength(2)]],
-      lastName:[null, [Validators.required, Validators.maxLength(100), Validators.minLength(2)]],
-      phoneNumber:[null,Validators.maxLength(20)],
-      whatsappNumber:[null, Validators.maxLength(20)],
-      adress:[null, [Validators.required, Validators.maxLength(100), Validators.minLength(2)]],
-      isClientEnGros: [false],
+      name: [data?.name, [Validators.required, Validators.maxLength(100), Validators.minLength(2)]],
+      lastName: [data?.lastName, [Validators.required, Validators.maxLength(100), Validators.minLength(2)]],
+      phoneNumber: [data?.phoneNumber, Validators.maxLength(20)],
+      whatsappNumber: [data?.whatsappNumber, Validators.maxLength(20)],
+      adress: [data?.adress, [Validators.required, Validators.maxLength(100), Validators.minLength(2)]],
+      isClientEnGros: [data?.isClientEnGros],
     })
   }
 
   save() {
     if (this.form.valid) {
-      console.log('test')
-        this.showSpinner = true;
-        if (this.data?.action === 'add') {
-            this._kadaService.createClient({ ...this.form.value })
-                .subscribe({
-                    next: (response) => {
-                        this.dialogRef.close(true);
-                        this._toastr.success("Client ajouté avec succès!");
-                    },
-                    error: (err) => {
-                        this._toastr.error(`Error! Veuillez vérifier vos champs puis réessayer!\n ${err.title}`);
-                        this.showSpinner = false;
-                    },
-                    complete: () => {
-                        this.showSpinner = false;
-                    }
-                })
-        }
+      this.showSpinner = true;
+      if (this.data?.action === 'add') {
+        this._kadaService.createClient({ ...this.form.value })
+          .subscribe({
+            next: (response) => {
+              this.dialogRef.close(true);
+              this._toastr.success("Client ajouté avec succès!");
+            },
+            error: (err) => {
+              this._toastr.error(`Error! Veuillez vérifier vos champs puis réessayer!\n ${err.title}`);
+              this.showSpinner = false;
+            },
+            complete: () => {
+              this.showSpinner = false;
+            }
+          })
+      } else {
+        this._kadaService.updateClient(this.data?.id,{ ...this.form.value,id:this.data?.id })
+          .subscribe({
+            next: () => {
+              this.dialogRef.close(true);
+              this._toastr.success("Client modifié avec succès!");
+            },
+            error: (err) => {
+              this._toastr.error(`Erreur! Veuillez réessayer!\n ${err.title}`);
+              this.showSpinner = false;
+            },
+            complete: () => {
+              this.showSpinner = false;
+            }
+          })
+      }
     }
-}
+  }
 }
